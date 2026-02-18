96445a670dfb7d31797b865a8f22945719d8288a
Brayan de Jesus de la Cruz Garcia <142959994+ALFALFA68@users.noreply.github.com>
2026-02-18 01:39:40 -0600

    prueba
    prueba cl

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/dashboard', function () {
     return view('welcome');
-});
+})->middleware('auth');
+
+Route::get('/login', function () {
+    return "Página de Login";
+})->name('login');
