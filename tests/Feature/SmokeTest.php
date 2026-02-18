2ffeddb120a6d629f55379294da35843ab96a627
Brayan de Jesus de la Cruz Garcia <142959994+ALFALFA68@users.noreply.github.com>
2026-02-18 02:05:18 -0600

    prueba en main 1

--- tests/Feature/SmokeTest.php
+++ tests/Feature/SmokeTest.php
@@ -19,6 +19,7 @@ class SmokeTest extends TestCase
         // Verificamos que el estado sea 302 (Redirección)
         $response->assertStatus(202);
         
+        
         // Verificamos que redirija específicamente a la página de login
         $response->assertRedirect('/login');
     }
